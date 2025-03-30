ea0b7980119a7b87b11eecb9912871b3718f5db3
aceone02 <spamforme665@gmail.com>
2025-03-30 11:34:26 +0300

    Create c42.js

---
+++ c42.js
@@ -0,0 +1,6 @@
+// emailConfig.js
+const emailConfig = {
+    service_id: "service_czptzwh",  // service_id
+    template_id: "template_d1wqe2a", // template_id
+    user_id: "VCV4jqBE8vedyJK-B"    // user_id
+};
